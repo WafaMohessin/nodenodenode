b5a60d299199b2992a6504600639c2c50f7aa0cc
Wafa Mohessin <wafamohessin@gmail.com>
2021-10-10 18:40:18 -0400

    working on my homework

--- index.js
+++ index.js
@@ -1,46 +1,114 @@
-const { fstat } = require('node:fs')
+
 
 const inquirer = require('inquirer')
+
+const fs = require('fs')
+
+console.log('inquirer')
+
+
+//name, location, hobby, food, linkedin URL, and GithubURL.
 
     inquirer
         .prompt([ 
             {
                 type:'input',
-                name:'username',
-                message:"what is your username?"
+                name:'name',
+                message:"what is your name?"
             },
 
             {
                 type:'input',
-                name:'',
-                message:"?"
+                name:'location',
+                message:"Where are you located?"
               }, 
               {
-                type:'confirm',
-                name:'recommend',
-                message:"Do you recommend the bootcamp ?"
+                type:'input',
+                name:'hobby',
+                message:"What is your favrite hobby?"
               },
               {
-                type:'checkbox',
-                name:'food',
-                message:'which food do you like?',
-                choices:['cheese','pizza','falafel','meet']
+              type:'input',
+              name:'food',
+              message:"What is your facrite food?"
+              },
+              {
+                type:'input',
+                name:'linkedin',
+                message:'what is the url to your linkedin profile?'               
+              },
+              {
+                type:'input',
+                name:'Github',
+                message:'what is the url to your Github?',
+               
               }
         ])
   
+        
+        .then (answers => {
+
+
+          let {name,location, hobby, food, linkedin,GithubURL} = answers
+          let html= `
+          
+          <!DOCTYPE html>
+          <html lang="en">
+          <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+            <title>Document</title>
+          </head>
+
+          <body>
+            <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+              <h1 class="display-4">Hi! My name is ${answers.name}</h1>
+              <p class="lead">I am from ${answers.location}.</p>
+              <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+
+              <ul class="list-group">
+                <li class="list-group-item">My GitHub username is ${answers.hobby}</li>
+                <li class="list-group-item">My GitHub username is ${answers.food}</li>
+                <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
+                <li class="list-group-item">LinkedIn: ${answers.GitHub}</li>                
+              </ul>
+            </div>
+          </div>
+          </body>
+          </html>`
+
+          console.log(html)
+
+          fs.writeFile('index.html', html, function(err){
+
+            if(err) throw err
+            console.log('file saved')
+          })
+        })
+
+
+        
+
+
+          /*console.log(html){
+            fs.writeFile("newFile.html",html,function(err){
+              if(err) throw err
+              console.log('file saved')
+            }
+          
+          
+          
+             .then((answers) => {
+              const htmlPageContent = generateHTML(answers);
+          
+              fs.writeFile('index.html', htmlPageContent, (err) =>
+                err ? console.log(err) : console.log('Successfully created index.html!')
+              );
+            });
+           */
 
     
-
-/* .then (answers => {
-    
-    console.log(answers)
-
-    const{username, age, recommend,food} = answers
-
-    const data= `Hi my name is ${username}. I am ${age}year old. I like ${food}}`
-
-    fs.writeFile('test.test', data ,err => console.log ('File saved'))
-
-  }) */
 
 
